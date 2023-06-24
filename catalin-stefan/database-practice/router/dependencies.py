c823cf36792777e437f5566bb50a5b5a4220b7a3
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-24 19:33:07 +0530

    added multi level dependency

--- catalin-stefan/database-practice/router/dependencies.py
+++ catalin-stefan/database-practice/router/dependencies.py
@@ -6,15 +6,26 @@ router=APIRouter(
     prefix="/dependencies",
     tags=["dependencies"]
 )
-#//dependency function
-def convert_headers(request:Request,separator:str="^^"):
+# Multi Level Dependencies
+def convert_params(request:Request,separator:str):
+    query=[]
+    for key,value in request.query_params.items():
+        query.append(f"{key} {separator} {value}")
+    return query
+
+# simple - dependency function
+# for multi level dependencies we are injecting another dependency to the dpendency function itself convert_params
+def convert_headers(request:Request,separator:str="^^", query=Depends(convert_params)):
     out_headers=[]
     for key,value in request.headers.items():
         out_headers.append(f"{key} __{separator}__ {value}")
-    return out_headers
+    return {
+        "out_headers":out_headers,
+        "query":query
+    }
 
 @router.get("")
-def get_items(separator:str='--',headers=Depends(convert_headers)):
+def get_items(test:str,separator:str='--',headers=Depends(convert_headers)):
     return{
         'items':['a','b','c'],
         'headers':headers
@@ -26,7 +37,7 @@ def create_item(headers=Depends(convert_headers)):
         'result':'new item created',
         'headers':headers
     }
-
+# class - dependency function
 # define a class and inject the class as a dependency
 class Account:
     def __init__(self,name:str,email:str):
