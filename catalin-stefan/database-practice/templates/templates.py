a071b1c420ce53a5b232b8cd986d8203f905e603
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-24 16:46:20 +0530

    added background tasks

--- catalin-stefan/database-practice/templates/templates.py
+++ catalin-stefan/database-practice/templates/templates.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
+from starlette.background import BackgroundTasks
+
 from schemas import ProductBase
+from custom_log import log
 
 router=APIRouter(
     prefix="/templates",
@@ -12,7 +15,8 @@ router=APIRouter(
 templates=Jinja2Templates(directory="templates")
 # get template
 @router.get("/products/{id}",response_class=HTMLResponse)
-def get_product(id:str,request:Request):
+def get_product(id:str,request:Request,bt:BackgroundTasks):
+    bt.add_task(log_template_call,f"Template read for product with id {id}")
     return templates.TemplateResponse(
         "product.html",
         {
@@ -20,3 +24,6 @@ def get_product(id:str,request:Request):
             "id":id
         }
     )
+
+def log_template_call(message:str):
+    log("MyApi",message)
