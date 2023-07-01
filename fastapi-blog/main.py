4a1e429565248b1da71a18780470fab0ccfd9518
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-01 23:41:30 +0530

    blog site fast api - added sqlite3 POST request

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -1,8 +1,11 @@
 # uvicorn main:app --port 8080  --reload
 from fastapi import FastAPI
+from database import models
+from database.database import engine
+from routers import post
 
 app=FastAPI()
+app.include_router(post.router)
 
-@app.get("/")
-def hw():
-    return "Hello World"
+# create db
+models.Base.metadata.create_all(engine)
