8b6adfc346f0c9624158bdd4b2b077a2cc6a180e
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-02 15:26:36 +0530

    blog site fast api - added image upload and CORS middleware

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -3,9 +3,27 @@ from fastapi import FastAPI
 from database import models
 from database.database import engine
 from routers import post
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 app.include_router(post.router)
 
 # create db
 models.Base.metadata.create_all(engine)
+
+# move files to a static path
+#  name="images" is a parameter of mount function call
+app.mount('/images',StaticFiles(directory="images"),name="images")
+
+origins=[
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
--- fastapi-blog/routers/post.py
+++ fastapi-blog/routers/post.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter,Depends
+import shutil
+
+from fastapi import APIRouter,Depends,File,UploadFile
 from routers.schemas import PostBase, PostDisplay
 from sqlalchemy.orm import Session
 from database.database import get_db
 from database import db_post
-
+import string,random
 
 router=APIRouter(
     prefix="/post",
@@ -21,3 +23,18 @@ def posts(db:Session=Depends(get_db)):
 @router.delete("/{id}")
 def delete(id:int,db:Session=Depends(get_db)):
     return db_post.delete(id,db)
+
+@router.post('/image')
+def upload_image(image:UploadFile=File(...)):
+    # generate random string of letters to add to the name of our file
+    letter=string.ascii_letters
+    # generate a string of 6 letters and add to the string
+    rand_str=''.join(random.choice(letter) for i in range(6))
+    # Append to file name
+    new = f'_{rand_str}.'
+    filename=new.join(image.filename.rsplit('.',1))
+    path=f'images/{filename}'
+    # write file to images and close the buffer
+    with open(path,"w+b") as buffer:
+        shutil.copyfileobj(image.file,buffer)
+        return {'filename':path}
