a549703970a71dc1bcfbfa776a7c73fdec4a4742
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-08 23:30:18 +0530

    git commit -m "Instagram clone - initial commit"

---
+++ catalin-stefan/instagram-clone/server/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from db import models
+from db.database import engine
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return "Hello World!"
+
+
+models.Base.metadata.create_all(engine)
