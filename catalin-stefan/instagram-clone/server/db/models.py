a991a809a343ddbcf5569e3358949e1adc1449e9
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-09 20:31:28 +0530

    git commit -m "added add user"

--- catalin-stefan/instagram-clone/server/db/models.py
+++ catalin-stefan/instagram-clone/server/db/models.py
@@ -1,6 +1,7 @@
 from .database import Base
 from sqlalchemy import Column,Integer,String
 
+# model schema for python backend
 class DbUser(Base):
     __tablename__="user"
     id=Column(Integer,primary_key=True,index=True)
