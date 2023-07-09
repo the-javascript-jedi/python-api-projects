d5d69b93c8668a799c3f298f10cc347145482ebf
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-09 21:07:05 +0530

    added hashing functions

--- catalin-stefan/instagram-clone/server/db/db_user.py
+++ catalin-stefan/instagram-clone/server/db/db_user.py
@@ -1,12 +1,15 @@
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
 from db.models import DbUser
+from db.hashing import Hash
 
 def create_user(db:Session,request:UserBase):
     new_user=DbUser(
         username=request.username,
         email=request.email,
-        password=request.password
+        # password=request.password
+        # hash the password
+        password=Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
