297007da733700bd495b223c095bbd964164cdb0
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-20 17:28:57 +0530

    updated the cors and added data for backend

--- catalin-stefan/instagram-clone/server/main.py
+++ catalin-stefan/instagram-clone/server/main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user,post,comment
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
@@ -17,6 +18,17 @@ app.include_router(authentication.router)
 def root():
     return "Hello World!"
 
+origins=[
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 models.Base.metadata.create_all(engine)
 
