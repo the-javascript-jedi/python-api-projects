9351a073dc9662d09cdc17a8a90c3b95eeb46592
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-11-28 20:32:43 +0530

    added docker file

--- youtube/docker-fastapi/main.py
+++ youtube/docker-fastapi/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+# get environment variables
+from os import environ as env
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"details":"Hello World!"}
+    return {"details": f"Hello World! secret={env['MY_VARIABLE']}"}
