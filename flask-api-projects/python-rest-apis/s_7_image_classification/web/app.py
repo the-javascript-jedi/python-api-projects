3c9a8ced4a9fb540677ebf7a99a275481d7f0127
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-05-10 08:59:23 +0530

    added api for register in image classification

--- flask-api-projects/python-rest-apis/s_7_image_classification/web/app.py
+++ flask-api-projects/python-rest-apis/s_7_image_classification/web/app.py
@@ -2,177 +2,59 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+import numpy as np
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
+#Initialize mongo db client
 client = MongoClient("mongodb://db:27017")
+
+# create a new db and collection
 db = client.SimilarityDB
 users = db["Users"]
 
-def UserExist(username):
-    # if users.find({"Username":username}).count() == 0:
-    if users.count_documents({"Username":username}) == 0:
+# check if user exists in db
+def user_exists(username):
+    if users.count_documents({"Username":username})==0:
         return False
     else:
         return True
 
 class Register(Resource):
     def post(self):
-        #Step 1 is to get posted data by the user
-        postedData = request.get_json()
+        # we first get the posted data
+        posted_data=request.get_json()
+        # get user name and password
+        username=posted_data["username"]
+        password=posted_data["password"]
 
-        #Get the data
-        username = postedData["username"]
-        password = postedData["password"] #"123xyz"
-
-        if UserExist(username):
-            retJson = {
-                'status':301,
-                'msg': 'Invalid Username'
+        # check if user already exists
+        if user_exists(username):
+            ret_json={
+                "status":301,
+                "message":"Invalid username,user already exists"
             }
-            return jsonify(retJson)
+            return jsonify(ret_json)
 
-        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        # if user is new hash password
+        hashed_pw=bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())
 
-        #Store username and pw into the database
+        # store the new user in db
         users.insert_one({
-            "Username": username,
-            "Password": hashed_pw,
-            "Tokens":6
+            "Username":username,
+            "Password":hashed_pw,
+            "Tokens":4
         })
 
-        retJson = {
-            "status": 200,
-            "msg": "You successfully signed up for the API"
-        }
-        return jsonify(retJson)
-
-def verifyPw(username, password):
-    if not UserExist(username):
-        return False
-
-    hashed_pw = users.find({
-        "Username":username
-    })[0]["Password"]
-
-    if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
-        return True
-    else:
-        return False
-
-def countTokens(username):
-    tokens = users.find({
-        "Username":username
-    })[0]["Tokens"]
-    return tokens
-
-class Detect(Resource):
-    def post(self):
-        #Step 1 get the posted data
-        postedData = request.get_json()
-
-        #Step 2 is to read the data
-        username = postedData["username"]
-        password = postedData["password"]
-        text1 = postedData["text1"]
-        text2 = postedData["text2"]
-
-        if not UserExist(username):
-            retJson = {
-                'status':301,
-                'msg': "Invalid Username"
+        # return success
+        ret_json={
+                "status":200,
+                "message":"You have successfully signed up for the API"
             }
-            return jsonify(retJson)
-        #Step 3 verify the username pw match
-        correct_pw = verifyPw(username, password)
+        return jsonify(ret_json)
+    
 
-        if not correct_pw:
-            retJson = {
-                "status":302,
-                "msg": "Incorrect Password"
-            }
-            return jsonify(retJson)
-        #Step 4 Verify user has enough tokens
-        num_tokens = countTokens(username)
-        if num_tokens <= 0:
-            retJson = {
-                "status": 303,
-                "msg": "You are out of tokens, please refill!"
-            }
-            return jsonify(retJson)
+api.add_resource(Register,'/register')
 
-        #Calculate edit distance between text1, text2
-        import spacy
-        nlp = spacy.load('en_core_web_sm')
-        text1 = nlp(text1)
-        text2 = nlp(text2)
-
-        # Ratio is a number between 0 and 1 the closer to 1, the more similar text 1 
-        # and text 2 are
-        ratio = text1.similarity(text2)
-
-        retJson = {
-            "status":200,
-            "ratio": ratio,
-            "msg":"Similarity score calculated successfully"
-        }
-
-        #Take away 1 token from user
-        current_tokens = countTokens(username)
-        users.update_one({
-            "Username":username
-        }, {
-            "$set":{
-                "Tokens":current_tokens-1
-                }
-        })
-
-        return jsonify(retJson)
-
-class Refill(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        username = postedData["username"]
-        password = postedData["admin_pw"]
-        refill_amount = postedData["refill"]
-
-        if not UserExist(username):
-            retJson = {
-                "status": 301,
-                "msg": "Invalid Username"
-            }
-            return jsonify(retJson)
-
-        correct_pw = "abc123"
-        if not password == correct_pw:
-            retJson = {
-                "status":304,
-                "msg": "Invalid Admin Password"
-            }
-            return jsonify(retJson)
-
-        #MAKE THE USER PAY!
-        users.update_one({
-            "Username":username
-        }, {
-            "$set":{
-                "Tokens":refill_amount
-                }
-        })
-
-        retJson = {
-            "status":200,
-            "msg": "Refilled successfully"
-        }
-        return jsonify(retJson)
-
-
-api.add_resource(Register, '/register')
-api.add_resource(Detect, '/detect')
-api.add_resource(Refill, '/refill')
-
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0')
